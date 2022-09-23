8f1fa42ebd70c4eaecc58e2f3f8f2953e6718cb0
Kenta Laptop <kenta.waibel@gmail.com>
2022-09-23 12:45:48 +0200

    updating files
    bz Kenta Waibeupdating files
    
    bz Kenta Waibell

--- _03_Cars/src/main/java/Car.java
+++ _03_Cars/src/main/java/Car.java
@@ -7,14 +7,20 @@ public class Car {
     //Attributes
     private String model;
     private double velocity;
+    private double cost;
+    private String color;
 
-    public Car(String model, double velocity) {
+    public Car(String model, double velocity, double cost, String color) {
         this.model = model;
         this.velocity = velocity;
+        this.cost = cost;
+        this.color = color;
     }
     public Car(){
         model = "unknown";
         velocity = 0;
+        cost = 0;
+        color = "unknown";
     }
 
     public String getModel() {
@@ -24,6 +30,12 @@ public class Car {
     public double getVelocity() {
         return velocity;
     }
+    public double getCost() {
+        return cost;
+    }
+    public String getColor() {
+        return color;
+    }
 
     public void setModel(String model) {
         this.model = model;
@@ -32,10 +44,16 @@ public class Car {
     public void setVelocity(double velocity) {
         this.velocity = velocity;
     }
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     @Override
     public String toString() {
-        return "Car{" + "model=" + model + '\'' + ", velocity=" + velocity + '}';
+        return "Car{" + "model=" + model + '\'' + ", velocity=" + velocity + '\'' + "cost=" + cost + '\'' + "color" + color +'}';
     }
     public void accelerate() {
         System.out.println("Car.accelerate(): is called, but no implementation available");
